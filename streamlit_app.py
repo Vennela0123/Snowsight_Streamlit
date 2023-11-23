96375d218ad92880bd489d91b427c0389066f7ea
Vennela0123 <151828611+Vennela0123@users.noreply.github.com>
2023-11-23 19:16:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,3 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-session = get_active_session()
-# sql = st.text_area("Query",value="select * from snowflake_sample_data.tpch_sf1.lineitem limit 10")
-# f"select * from snowflake_sample_data.tpch_sf1.lineitem limit 20"
-sql='SELECT EMP_CTR AS "Category",COUNT(*) AS "No Of Employees" FROM STREAMLIT.PUBLIC.MANPOWER GROUP BY 1'
-data=session.sql(sql).collect()
-if st.button("Execute Query") :
-    if data:
-            st.success("Query executed ")
-            st.write("Result:")
-            st.area_chart(data)
-    else:
-            st.warning("No data retrieved")
+import snowflake.connector
+st.title("Snowflake Streamlit App")
